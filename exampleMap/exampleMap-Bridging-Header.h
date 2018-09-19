953e012f0bce6bfca50f1a9c95c247d8a11f8ff3
jhninja <jhninja@naver.com>
2018-09-19 14:49:51 +0900

    initaal

---
+++ exampleMap/exampleMap-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  exampleMap-Bridging-Header.h
+//  exampleMap
+//
+//  Created by 김지훈 on 2018. 9. 19..
+//  Copyright © 2018년 김지훈. All rights reserved.
+//
+#ifndef exampleMap_Bridging_Header_h
+#define exampleMap_Bridging_Header_h
+
+#import <NMapViewerSDK/NMapView.h>
+#import <NMapViewerSDK/NMapLocationManager.h>
+
+#endif
